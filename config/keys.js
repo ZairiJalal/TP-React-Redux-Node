2deb5971946eda87e7ae7d0a070de42f4b7c061b
ZairiJalal <zairi.jalal98@gmail.com>
2021-04-10 13:22:45 +0200

    first commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI : 'mongodb+srv://ZairiJalal:okokok2017@cluster0.amgm0.mongodb.net/test?retryWrites=true&w=majority'
+}
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const items = require('./routes/api/items');
+const app = express();
+
+app.use(bodyParser.json());
+
+const db = require('./config/keys').mongoURI;
+
+mongoose.connect(db)
+        .then(()=> console.log('Mongoose connected .....'))
+        .catch( err=>console.log(err) );
+
+
+app.use('/api/items', items);
+
+const port = process.env.port || 5000;
+
+app.listen(port,()=> console.log(`Server started on port${port}`));
+
