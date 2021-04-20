2e9079109406c716a0fd6f97af421578b17f3f11
ZairiJalal <zairi.jalal98@gmail.com>
2021-04-20 19:19:37 +0200

    react client

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AppNavBar from './components/AppNavBar';
+import ShoppingList from './components/ShoppingList';
+
+
+function App() {
+  return (
+    <div className="App">
+      <AppNavBar/>
+      <ShoppingList/>
+    </div>
+  );
+}
+
+export default App;
